8af150efbfc1e689c271f02d41fb857e0f4b7aa5
PacmanLazy <beloved524@gmail.com>
2017-08-10 13:59:41 +0300

    some comments for you

--- Login/static/Login/js/validate_first_access.js
+++ Login/static/Login/js/validate_first_access.js
@@ -1,7 +1,9 @@
+// getting current script
 var scripts = document.getElementsByTagName("script");
 var current_script = scripts[scripts.length-1];
 var script_object = $(current_script);
 
+// receiving data params
 var url = script_object.attr("data-url");
 var loginId = script_object.attr("data-LoginID");
 var passwordId = script_object.attr("data-PasswordID");
@@ -9,6 +11,7 @@ var confirmPassId = script_object.attr("data-ConfirmPassID");
 
 var error_message = "Пароли не совпадают!";
 
+// login validation function
 $("#".concat(loginId)).change(function () {
     var login_value = $(this).val();
     $.ajax({
@@ -23,6 +26,7 @@ $("#".concat(loginId)).change(function () {
     });
 });
 
+// checking password confirmation
 $("#".concat(confirmPassId)).change(function () {
     var new_pass = $("#".concat(passwordId)).val();
     var conf_pass = $(this).val();
