87d00c134288c2c8a250c4002d83273689e07b93
PacmanLazy <beloved524@gmail.com>
2017-09-05 16:08:11 +0300

    Task Models is added to admin panel

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(Document)
+admin.site.register(Task_doc)
+admin.site.register(Subject)
+admin.site.register(Teacher_Subject)
